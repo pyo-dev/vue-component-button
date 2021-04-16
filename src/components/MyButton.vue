bbae0874d70c708c7e13d86882a69dd6e4c77171
표정민 <>
2021-04-16 13:10:59 +0900

    setting

---
+++ src/components/MyButton.vue
@@ -0,0 +1,110 @@
+<template>
+    <button :class="addClass" :style="addStyle" @click="onClick"><slot>확인</slot></button>
+</template>
+
+<script>
+export default {
+    name: 'MyButton',
+
+    data() {
+        return {
+            CLASS_NAME: 'pyo-bt'
+        }
+    },
+
+    props: {
+        size: {
+            type: String,
+            default: 'medium',
+        },
+        font_size: {
+            type: String,
+            default: '',
+        },
+        font_weigth: {
+            type: String,
+            default: '',
+        },
+        font_color: {
+            type: String,
+            default: '',
+        },
+        border_color: {
+            type: String,
+            default: '',
+        },
+        border_size: {
+            type: String,
+            default: '',
+        },
+        fill: {
+            type: Boolean,
+            default: false,
+        },
+        fill_color: {
+            type: String,
+            default: '',
+        },
+        padding: {
+            type: String,
+            default: '',
+        },
+        
+    },
+
+    computed: {
+        addClass() {
+            return {
+                [this.CLASS_NAME] : true,
+                [this.CLASS_NAME + '-fill'] : this.fill,
+                [this.CLASS_NAME + '-' + this.size] : ['small', 'large'].indexOf(this.size) !== -1,
+            };
+        },
+        addStyle() {
+            return {
+                'padding': this.padding,
+                'border-color': this.border_color || this.fill_color,
+                'border-width': this.border_size,
+                'background': this.fill_color,
+                'font-size': this.font_size,
+                'font-weight': this.font_weigth,
+                'color': this.font_color,
+            };
+        },
+    },
+
+    methods: {
+        onClick() {
+            this.$emit('onClick');
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+$class-name: 'pyo-bt';
+.#{$class-name}{
+    line-height: 1;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    background-color: transparent;
+    color: #000;
+    font-size: 14px;
+    outline: 0;
+    cursor: pointer;
+    &.#{$class-name}-fill{
+        border-color: #000;
+        background-color: #000;
+        color: #fff;
+    }
+    &.#{$class-name}-small{
+        padding: 5px 10px;
+        font-size: 12px;
+    }
+    &.#{$class-name}-large{
+        padding: 12px 25px;
+        font-size: 16px;
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,164 @@
+<template>
+    <div class="wrap">
+        <div class="box">
+            <div class="title">slot</div>
+            <MyButton />
+            <MyButton>취소</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">size</div>
+            <MyButton 
+                :size="'small'"
+            >small</MyButton>
+            <MyButton>default</MyButton>
+            <MyButton 
+                :size="'large'"
+            >large</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">font-size</div>
+            <MyButton>default</MyButton>
+            <MyButton 
+                :font_size="'20px'"
+            >20px</MyButton>
+            <MyButton 
+                :font_size="'2rem'"
+            >2rem</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">font-weigth</div>
+            <MyButton>default</MyButton>
+            <MyButton
+                :font_weigth="'bold'"
+            >bold</MyButton>
+            <MyButton
+                :font_weigth="'900'"
+            >900</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">font-color</div>
+            <MyButton>default</MyButton>
+            <MyButton
+                :font_color="'red'"
+            >red</MyButton>
+            <MyButton
+                :font_color="'#777'"
+            >#777</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">fill</div>
+            <MyButton 
+                :fill="true"
+            ></MyButton>
+            <MyButton 
+                :fill="true"
+                :fill_color="'red'"
+            >red</MyButton>
+            <MyButton 
+                :fill="true"
+                :fill_color="'#777'"
+            >#777</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">border-color</div>
+            <MyButton>default</MyButton>
+            <MyButton 
+                :border_color="'red'"
+            >red</MyButton>
+            <MyButton 
+                :border_color="'#777'"
+            >#777</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">border-size</div>
+            <MyButton>default</MyButton>
+            <MyButton 
+                :border_size="'3px'"
+            >3px</MyButton>
+            <MyButton 
+                :border_size="'1px 3px 3px 1px'"
+            >1px 3px 3px 1px</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">padding</div>
+            <MyButton>default</MyButton>
+            <MyButton 
+                :padding="'5px'"
+            >5px</MyButton>
+            <MyButton 
+                :padding="'20px 40px'"
+            >20px 40px</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">style total</div>
+            <MyButton 
+                :padding="'15px 25px'"
+                :border_color="'#E65320'"
+                :border_size="'1px 3px 3px 1px'"
+                :fill_color="'#F07522'"
+                :font_color="'#fff'"
+                :font_weight="'bold'"
+                :font_size="'18px'"
+            ></MyButton>
+            <MyButton 
+                :padding="'15px 25px'"
+                :border_color="'#061161'"
+                :border_size="'1px 3px 3px 1px'"
+                :fill_color="'linear-gradient(45deg, #061161, #780206)'"
+                :font_color="'#fff'"
+                :font_weight="'bold'"
+                :font_size="'18px'"
+            >취소</MyButton>
+        </div>
+        <div class="box">
+            <div class="title">click</div>
+            <MyButton @onClick="clickSuccess">success</MyButton>
+            <MyButton @onClick="clickFail">fail</MyButton>
+        </div>
+    </div>
+</template>
+
+<script>
+import MyButton from "@/components/MyButton";
+export default {
+    name: 'Hmoe',
+
+    components: {
+        MyButton
+    },
+
+    methods: {
+        clickSuccess() {
+            alert('success');
+        },
+        clickFail(){
+            alert('fail');
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: graid;
+}
+.wrap{
+    width: 500px;
+    margin: 0 auto;
+    .box{
+        padding: 20px;
+        margin-bottom: 20px;
+        border: 1px solid #ddd;
+        .title{
+            margin-bottom: 15px;
+            font-size: 18px;
+        }
+        button{
+            margin: 0 10px;
+        }
+    }
+}
+</style>
